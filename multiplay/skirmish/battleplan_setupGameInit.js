e80680f989f8941d4995fc7a53da60a5c6deef8a
Karamel <karamel@cupnplategames.com>
2020-04-06 17:59:21 +0200

    Initialization.

---
+++ multiplay/skirmish/battleplan_setupGameInit.js
@@ -0,0 +1,326 @@
+// This is the replaced function from rules.js
+// that defines which components and technologies are available from start.
+
+function removeModules(playnum)
+{
+	var structs = enumStruct(playnum);
+	for (var i = 0; i < structs.length; i++)
+	{
+		var s = structs[i];
+		if (s.modules > 0)
+		{
+			var x = s.x * 128;
+			var y = s.y * 128;
+			var type = s.stattype;
+			var name = "";
+			switch (s.stattype)
+			{
+			case RESEARCH_LAB:
+				name = "A0ResearchFacility";
+				break;
+			case FACTORY:
+				name = "A0LightFactory"
+				break;
+			case POWER_GEN:
+				name = "A0PowerGenerator";
+				break;
+			}
+			if (name != "")
+			{
+				removeObject(s);
+				addStructure(name, playnum, x, y);
+			}
+		}
+	}
+}
+
+function limitStartingFacilities(playnum)
+{
+	var max_labs = 1;
+	var max_facto = 1;
+	var max_vtol = 1;
+	var max_cyb = 0;
+	switch (baseType)
+	{
+	case CAMP_CLEAN:
+		max_labs = 1;
+		max_facto = 0;
+		max_vtol = 0;
+		break;
+	case CAMP_BASE:
+		max_labs = 1;
+		max_facto = 1;
+		max_vtol = 1;
+		break;
+	default:
+		return;
+	}
+	var count_labs = 0;
+	var count_facto = 0;
+	var count_vtol = 0;
+	var count_cyb = 0;
+	var structs = enumStruct(playnum, RESEARCH_LAB);
+	for (var i = 0; i < structs.length; i++)
+	{
+		var s = structs[i];
+		count_labs++;
+		if (count_labs > max_labs)
+			removeObject(s);
+	}
+	var structs = enumStruct(playnum, CYBORG_FACTORY);
+	for (var i = 0; i < structs.length; i++)
+	{
+		var s = structs[i];
+		count_cyb++;
+		if (count_cyb > max_cyb)
+			removeObject(s);
+	}
+	var structs = enumStruct(playnum, VTOL_FACTORY);
+	for (var i = 0; i < structs.length; i++)
+	{
+		var s = structs[i];
+		count_vtol++;
+		if (count_vtol > max_cyb)
+			removeObject(s);
+	}
+	structs = enumStruct(playnum, FACTORY);
+	for (var i = 0; i < structs.length; i++)
+	{
+		var s = structs[i];
+		count_facto++;
+		if (count_facto > max_facto)
+		{
+			var x = s.x * 128;
+			var y = s.y * 128;
+			removeObject(s);
+			if (count_cyb < max_cyb)
+			{
+				addStructure("A0CyborgFactory", playnum, x, y);
+				count_vtol++;
+			}
+			else if (count_vtol < max_vtol)
+			{
+				addStructure("A0VTolFactory1", playnum, x, y);
+				count_vtol++;
+			}
+		}
+	}
+
+}
+
+function addStartingScout(playnum)
+{
+	// Put it near the CC
+	var structs = enumStruct(playnum, HQ);
+	if (structs.length == 0)
+		return;
+	var ccPos = {x: structs[0].x, y: structs[0].y};
+	var tries = 0;
+	var offset_y = 0;
+	var offset_x = 0;
+	while (tries < 12)
+	{
+		if (tries < 4)
+			offset_y = 2
+		else if (tries < 6)
+			offset_y = 1
+		else if (tries < 8)
+			offset_y = 0
+		else
+			offset_y = -1
+		if (tries < 4 || tries >= 8)
+			offset_x = 2 - (tries % 4)
+		else if (tries < 8)
+			offset_x = 2 - (tries % 2) * 3
+		if (addDroid(playnum, (ccPos.x + offset_x) - 1, (ccPos.y + offset_y) - 1,
+				"Scout", "Body4ABT", "hover01", 0, 0, "Scout") == null)
+			tries++;
+		else
+			return;
+	}
+}
+
+function eventGameInit()
+{
+	setupGame();
+
+	// always at least one oil drum, and one more for every 64x64 tiles of map area
+	maxOilDrums = (mapWidth * mapHeight) >> 12; // replace float division with shift for sync-safety
+	for (var i = 0; i < maxOilDrums; ++i)
+	{
+		queue("placeOilDrum", 10000 * i);
+	}
+
+	hackNetOff();
+	makeComponentAvailable("B4body-sml-trike01", scavengerPlayer);
+	makeComponentAvailable("B3body-sml-buggy01", scavengerPlayer);
+	makeComponentAvailable("B2JeepBody", scavengerPlayer);
+	makeComponentAvailable("BusBody", scavengerPlayer);
+	makeComponentAvailable("FireBody", scavengerPlayer);
+	makeComponentAvailable("B1BaBaPerson01", scavengerPlayer);
+	makeComponentAvailable("BaBaProp", scavengerPlayer);
+	makeComponentAvailable("BaBaLegs", scavengerPlayer);
+	makeComponentAvailable("bTrikeMG", scavengerPlayer);
+	makeComponentAvailable("BuggyMG", scavengerPlayer);
+	makeComponentAvailable("BJeepMG", scavengerPlayer);
+	makeComponentAvailable("BusCannon", scavengerPlayer);
+	makeComponentAvailable("BabaFlame", scavengerPlayer);
+	makeComponentAvailable("BaBaMG", scavengerPlayer);
+	for (var playnum = 0; playnum < maxPlayers; playnum++)
+	{
+		if (powerType == 0)
+		{
+			setPowerModifier(85, playnum);
+		}
+		else if (powerType == 2)
+		{
+			setPowerModifier(125, playnum);
+		}
+
+		// insane difficulty is meant to be insane...
+		if (playerData[playnum].difficulty == INSANE)
+		{
+			setPowerModifier(200 + 15 * powerType, playnum);
+		}
+		else if (playerData[playnum].difficulty == EASY)
+		{
+			setPowerModifier(70 + 5 * powerType, playnum);
+		}
+
+		setDroidLimit(playnum, 150, DROID_ANY);
+		setDroidLimit(playnum, 10, DROID_COMMAND);
+		setDroidLimit(playnum, 15, DROID_CONSTRUCT);
+
+		enableStructure("A0CommandCentre", playnum);		// make structures available to build
+		enableStructure("A0LightFactory", playnum);
+		enableStructure("A0VTolFactory1", playnum);
+		enableStructure("A0ResourceExtractor", playnum);
+		enableStructure("A0PowerGenerator", playnum);
+		enableStructure("A0ResearchFacility", playnum);
+
+		setStructureLimits("A0LightFactory", 10, playnum);	// set structure limits
+		setStructureLimits("A0PowerGenerator", 8, playnum);
+		setStructureLimits("A0ResearchFacility", 5, playnum);
+		setStructureLimits("A0CommandCentre", 1, playnum);
+		setStructureLimits("A0ComDroidControl", 1, playnum);
+		setStructureLimits("A0CyborgFactory", 0, playnum);
+		setStructureLimits("A0VTolFactory1", 5, playnum);
+	}
+	applyLimitSet();	// set limit options
+
+	const numCleanTech = 8;	// do x for clean
+	const numBaseTech = numCleanTech + 4; // do x for base
+	var techlist = new Array(
+		"R-Vehicle-Prop-Wheels",
+		"R-Vehicle-Prop-Tracks",
+		"R-Vehicle-Prop-Hover",
+		"R-Vehicle-Prop-VTOL",
+		"R-Vehicle-Body04",
+		"R-Vehicle-Body01",
+		"R-Vehicle-Body02",
+		"R-Sys-Spade1Mk1",
+
+		"R-Wpn-MG1Mk1",
+		"R-Wpn-Cannon1Mk1",
+		"R-Wpn-Rocket05-MiniPod",
+		"R-Defense-TankTrap01",
+
+		"R-Wpn-Flamer01Mk1",
+		"R-Wpn-Rocket01-LtAT",
+		"R-Wpn-Mortar01Lt",
+		"R-Wpn-HowitzerMk1",
+		"R-Defense-HardcreteWall",
+		"R-Defense-Tower01",
+		"R-Defense-Tower-Cannon1Mk1",
+		"R-Defense-Tower06");
+
+	for (var playnum = 0; playnum < maxPlayers; playnum++)
+	{
+		makeComponentAvailable("Scout", playnum);
+		makeComponentAvailable("Scout-VTOL", playnum);
+		for (var technum = numCleanTech; technum < numBaseTech; technum++) {
+			enableResearch(techlist[technum]);
+		}
+		enableResearch("R-Wpn-Rocket01-LtAT");
+		enableResearch("R-Wpn-Mortar01Lt");
+		enableResearch("R-Wpn-HowitzerMk1");
+
+		if (baseType == CAMP_CLEAN)
+		{
+			setPower(1300, playnum);
+			for (var count = 0; count < numCleanTech; count++)
+			{
+				completeResearch(techlist[count], playnum);
+			}
+			var structs = enumStruct(playnum);
+			var lab_count = 0;
+			for (var i = 0; i < structs.length; i++)
+			{
+				var s = structs[i];
+				if (s.stattype != HQ && (playerData[playnum].difficulty != INSANE
+				    || (s.stattype != WALL && s.stattype != DEFENSE && s.stattype != GATE
+				        && s.stattype != RESOURCE_EXTRACTOR)))
+				{
+					if (s.stattype != RESEARCH_LAB)
+						removeObject(s, false);
+					else
+					{
+						lab_count++;
+						if (lab_count > 1)
+							removeObject(s, false);
+					}
+				}
+			}
+			removeModules(playnum);
+			limitStartingFacilities(playnum);
+		}
+		else if (baseType == CAMP_BASE)
+		{
+			setPower(2500, playnum);
+			for (var count = 0; count < numBaseTech; count++)
+			{
+				completeResearch(techlist[count], playnum);
+			}
+			// Keep only some structures
+			var structs = enumStruct(playnum);
+			for (var i = 0; i < structs.length; i++)
+			{
+				var s = structs[i];
+				if ((playerData[playnum].difficulty != INSANE && (s.stattype == WALL || s.stattype == DEFENSE))
+				    || s.stattype == GATE || s.stattype == CYBORG_FACTORY || s.stattype == COMMAND_CONTROL)
+				{
+					removeObject(s, false);
+				}
+			}
+			removeModules(playnum);
+			limitStartingFacilities(playnum);
+		}
+		else // CAMP_WALLS
+		{
+			setPower(2500, playnum);
+			for (var count = 0; count < techlist.length; count++)
+			{
+				completeResearch(techlist[count], playnum);
+			}
+			removeModules(s, playnum);
+		}
+		addStartingScout(playnum);
+	}
+
+	var techLevel = getMultiTechLevel();
+	if (techLevel == 2)
+	{
+		grantTech(TECH_TWO);
+	}
+	else if (techLevel == 3)
+	{
+		grantTech(TECH_THREE);
+	}
+
+	hackNetOn();
+	setTimer("checkEndConditions", 3000);
+	if (tilesetType === "URBAN" || tilesetType === "ROCKIES")
+	{
+		setTimer("weatherCycle", 45000);
+	}
+}
---
+++ multiplay/skirmish/rules.js
@@ -0,0 +1,463 @@
+//
+// Skirmish Base Script.
+//
+// contains the rules for starting and ending a game.
+// as well as warning messages.
+//
+// /////////////////////////////////////////////////////////////////
+
+receiveAllEvents(true);  // If doing this in eventGameInit, it seems to be too late in T2/T3, due to some eventResearched events triggering first.
+
+include("multiplay/script/camTechEnabler.js");
+include("multiplay/script/weather.js");
+
+var lastHitTime = 0;
+var cheatmode = false;
+var maxOilDrums = 0;
+var mainReticule = false;
+
+const CREATE_LIKE_EVENT = 0;
+const DESTROY_LIKE_EVENT = 1;
+const TRANSFER_LIKE_EVENT = 2;
+
+function reticuleManufactureCheck()
+{
+	var structureComplete = false;
+	var facs = [FACTORY, CYBORG_FACTORY, VTOL_FACTORY,];
+
+	for (var i = 0, len = facs.length; i < len; ++i)
+	{
+		var onMapFacs = enumStruct(selectedPlayer, facs[i]);
+		for (var j = 0, len2 = onMapFacs.length; j < len2; ++j)
+		{
+			if (onMapFacs[j].status === BUILT)
+			{
+				structureComplete = true;
+				break;
+			}
+		}
+	}
+
+	if (structureComplete === true)
+	{
+		setReticuleButton(1, _("Manufacture (F1)"), "image_manufacture_up.png", "image_manufacture_down.png");
+	}
+	else
+	{
+		setReticuleButton(1, _("Manufacture - build factory first"), "", "");
+	}
+}
+
+function reticuleResearchCheck()
+{
+	var structureComplete = false;
+	var labs = [RESEARCH_LAB,];
+
+	for (var i = 0, len = labs.length; i < len; ++i)
+	{
+		var onMapResLabs = enumStruct(selectedPlayer, labs[i]);
+		for (var j = 0, len2 = onMapResLabs.length; j < len2; ++j)
+		{
+			if (onMapResLabs[j].status === BUILT)
+			{
+				structureComplete = true;
+				break;
+			}
+		}
+	}
+	if (structureComplete === true)
+	{
+		setReticuleButton(2, _("Research (F2)"), "image_research_up.png", "image_research_down.png");
+	}
+	else
+	{
+		setReticuleButton(2, _("Research - build research facility first"), "", "");
+	}
+}
+
+function reticuleBuildCheck()
+{
+	if (enumDroid(selectedPlayer, DROID_CONSTRUCT).length > 0)
+	{
+		setReticuleButton(3, _("Build (F3)"), "image_build_up.png", "image_build_down.png");
+	}
+	else
+	{
+		setReticuleButton(3, _("Build - manufacture constructor droids first"), "", "");
+	}
+}
+
+function reticuleDesignCheck()
+{
+	var structureComplete = false;
+	var HQS = [HQ,];
+
+	for (var i = 0, len = HQS.length; i < len; ++i)
+	{
+		var onMapHQ = enumStruct(selectedPlayer, HQS[i]);
+		for (var j = 0, len2 = onMapHQ.length; j < len2; ++j)
+		{
+			if (onMapHQ[j].status === BUILT)
+			{
+				structureComplete = true;
+				break;
+			}
+		}
+	}
+	if (structureComplete === true)
+	{
+		setReticuleButton(4, _("Design (F4)"), "image_design_up.png", "image_design_down.png");
+		setMiniMap(true);
+		setDesign(true);
+	}
+	else
+	{
+		setReticuleButton(4, _("Design - construct HQ first"), "", "");
+		setMiniMap(false);
+	}
+}
+
+function reticuleCommandCheck()
+{
+	if (enumDroid(selectedPlayer, DROID_COMMAND).length > 0)
+	{
+		setReticuleButton(6, _("Commanders (F6)"), "image_commanddroid_up.png", "image_commanddroid_down.png");
+	}
+	else
+	{
+		setReticuleButton(6, _("Commanders - manufacture commanders first"), "", "");
+	}
+}
+
+function setMainReticule()
+{
+	setReticuleButton(0, _("Close"), "image_cancel_up.png", "image_cancel_down.png");
+	reticuleManufactureCheck();
+	reticuleResearchCheck();
+	reticuleBuildCheck();
+	reticuleDesignCheck();
+	setReticuleButton(5, _("Intelligence Display (F5)"), "image_intelmap_up.png", "image_intelmap_down.png");
+	reticuleCommandCheck();
+}
+
+function reticuleUpdate(obj, eventType)
+{
+	var update_reticule = false;
+
+	if (eventType === TRANSFER_LIKE_EVENT)
+	{
+		update_reticule = true;
+	}
+	else if (obj.player === selectedPlayer && obj.type === STRUCTURE)
+	{
+		if (obj.stattype === HQ || obj.stattype === RESEARCH_LAB || obj.stattype === CYBORG_FACTORY ||
+			obj.stattype === VTOL_FACTORY || obj.stattype === FACTORY || obj.stattype === COMMAND_CONTROL)
+		{
+			update_reticule = true;
+		}
+	}
+	else if (obj.player === selectedPlayer && obj.type === DROID)
+	{
+		if (obj.droidType === DROID_CONSTRUCT || obj.droidType === DROID_COMMAND)
+		{
+			update_reticule = true;
+		}
+	}
+
+	if (mainReticule && update_reticule)
+	{
+		//Wait a tick for the counts to update
+		const TICK_TIME = 100;
+		queue("setMainReticule", TICK_TIME);
+	}
+}
+
+function setupGame()
+{
+	if (tilesetType == "URBAN")
+	{
+		replaceTexture("page-8-player-buildings-bases.png", "page-8-player-buildings-bases-urban.png");
+		replaceTexture("page-9-player-buildings-bases.png", "page-9-player-buildings-bases-urban.png");
+		replaceTexture("page-7-barbarians-arizona.png", "page-7-barbarians-urban.png");
+	}
+	else if (tilesetType == "ROCKIES")
+	{
+		replaceTexture("page-8-player-buildings-bases.png", "page-8-player-buildings-bases-rockies.png");
+		replaceTexture("page-9-player-buildings-bases.png", "page-9-player-buildings-bases-rockies.png");
+		replaceTexture("page-7-barbarians-arizona.png", "page-7-barbarians-kevlar.png");
+		// for some reason rockies will use arizona babas
+	}
+	if (tilesetType != "ARIZONA")
+	{
+		setSky("texpages/page-25-sky-urban.png", 0.5, 10000.0);
+	}
+	// Disabled by default
+	setMiniMap(false);
+	setDesign(false);
+
+	setMainReticule();
+	showInterface();
+	mainReticule = true;
+	hackPlayIngameAudio();
+}
+
+function eventGameLoaded()
+{
+	setupGame();
+}
+
+include("multiplay/skirmish/battleplan_setupGameInit.js");
+
+// /////////////////////////////////////////////////////////////////
+// END CONDITIONS
+function checkEndConditions()
+{
+	var factories = countStruct("A0LightFactory") + countStruct("A0CyborgFactory");
+	var droids = countDroid(DROID_ANY);
+
+	// Losing Conditions
+	if (droids == 0 && factories == 0)
+	{
+		var gameLost = true;
+
+		/* If teams enabled check if all team members have lost  */
+		if (alliancesType == ALLIANCES_TEAMS || alliancesType == ALLIANCES_UNSHARED)
+		{
+			for (var playnum = 0; playnum < maxPlayers; playnum++)
+			{
+				if (playnum != selectedPlayer && allianceExistsBetween(selectedPlayer, playnum))
+				{
+					factories = countStruct("A0LightFactory", playnum) + countStruct("A0CyborgFactory", playnum);
+					droids = countDroid(DROID_ANY, playnum);
+					if (droids > 0 || factories > 0)
+					{
+						gameLost = false;	// someone from our team still alive
+						break;
+					}
+				}
+			}
+		}
+
+		if (gameLost)
+		{
+			gameOverMessage(false);
+			removeTimer("checkEndConditions");
+			return;
+		}
+	}
+
+	// Winning Conditions
+	var gamewon = true;
+
+	// check if all enemies defeated
+	for (var playnum = 0; playnum < maxPlayers; playnum++)
+	{
+		if (playnum != selectedPlayer && !allianceExistsBetween(selectedPlayer, playnum))	// checking enemy player
+		{
+			factories = countStruct("A0LightFactory", playnum) + countStruct("A0CyborgFactory", playnum); // nope
+			droids = countDroid(DROID_ANY, playnum);
+			if (droids > 0 || factories > 0)
+			{
+				gamewon = false;	//one of the enemies still alive
+				break;
+			}
+		}
+	}
+
+	if (gamewon)
+	{
+		gameOverMessage(true);
+		removeTimer("checkEndConditions");
+	}
+}
+
+// /////////////////////////////////////////////////////////////////
+// WARNING MESSAGES
+// Base Under Attack
+function eventAttacked(victimObj, attackerObj)
+{
+	if (gameTime > lastHitTime + 5000 && victimObj.player == selectedPlayer)
+	{
+		lastHitTime = gameTime;
+		if (victimObj.type == STRUCTURE)
+		{
+			playSound("pcv337.ogg", victimObj.x, victimObj.y, victimObj.z);	// show position if still alive
+		}
+		else
+		{
+			playSound("pcv399.ogg", victimObj.x, victimObj.y, victimObj.z);
+		}
+	}
+}
+
+function eventDroidBuilt(droid, structure)
+{
+	if (droid.player === selectedPlayer)
+	{
+		reticuleUpdate(droid, CREATE_LIKE_EVENT);
+	}
+}
+
+function eventStructureBuilt(struct, droid)
+{
+	if (struct.player === selectedPlayer)
+	{
+		reticuleUpdate(struct, CREATE_LIKE_EVENT);
+	}
+}
+
+function eventStructureDemolish(struct, droid)
+{
+	if (struct.player === selectedPlayer)
+	{
+		reticuleUpdate(struct, DESTROY_LIKE_EVENT);
+	}
+}
+
+function eventDestroyed(victim)
+{
+	if (victim.player === selectedPlayer)
+	{
+		reticuleUpdate(victim, DESTROY_LIKE_EVENT);
+	}
+}
+
+function eventObjectTransfer(obj, from)
+{
+	if (obj.player === selectedPlayer || from === selectedPlayer)
+	{
+		reticuleUpdate(obj, TRANSFER_LIKE_EVENT);
+	}
+}
+
+function eventResearched(research, structure, player)
+{
+	//if (research.name == "") debug("RESEARCH : " + research.fullname + "(" + research.name + ") for " + player);
+	// iterate over all results
+	for (var i = 0; i < research.results.length; i++)
+	{
+		var v = research.results[i];
+		//if (research.name == "") debug("    RESULT : class=" + v['class'] + " parameter=" + v['parameter'] + " value=" + v['value'] + " filter=" + v['filterParameter'] + " filterval=" + v['filterValue']);
+		for (var cname in Upgrades[player][v['class']]) // iterate over all components of this type
+		{
+			var parameter = v['parameter'];
+			var ctype = v['class'];
+			var filterparam = v['filterParameter'];
+			if ('filterParameter' in v && Stats[ctype][cname][filterparam] != v['filterValue']) // more specific filter
+			{
+				//if (research.name == "") debug("    skipped param=" + parameter + " cname=" + cname);
+				continue;
+			}
+			if (Stats[ctype][cname][parameter] instanceof Array)
+			{
+				var dst = Upgrades[player][ctype][cname][parameter].slice();
+				for (var x = 0; x < dst.length; x++)
+				{
+					dst[x] += Math.ceil(Stats[ctype][cname][parameter][x] * v['value'] / 100);
+				}
+				Upgrades[player][ctype][cname][parameter] = dst;
+				//debug("    upgraded to " + dst);
+			}
+			else if (Stats[ctype][cname][parameter] > 0) // only applies if stat has above zero value already
+			{
+				Upgrades[player][ctype][cname][parameter] += Math.ceil(Stats[ctype][cname][parameter] * v['value'] / 100);
+				//if (research.name == "") debug("      upgraded " + cname + " to " + Upgrades[player][ctype][cname][parameter] + " by " + Math.ceil(Stats[ctype][cname][parameter] * v['value'] / 100));
+			}
+			//else if (research.name == "") debug("    passed " + Stats[ctype][cname][parameter] + " param=" + parameter + " cname=" + cname);
+		}
+	}
+}
+
+function eventCheatMode(entered)
+{
+	cheatmode = entered; // remember this setting
+}
+
+function eventChat(from, to, message)
+{
+	if (message == "bettertogether" && cheatmode)
+	{
+		for (var i in Upgrades[from].Brain)
+		{
+			if (Upgrades[from].Brain[i].BaseCommandLimit > 0) // is commander
+			{
+				Upgrades[from].Brain[i].BaseCommandLimit += 4;
+				Upgrades[from].Brain[i].CommandLimitByLevel += 2;
+				// you must set the thresholds this way, as an array, because of the clunky
+				// way that this is implemented behind the scenes
+				Upgrades[from].Brain[i].RankThresholds = [ 0, 2, 4, 8, 16, 24, 32, 48, 64 ];
+			}
+		}
+		console("Made player " + from + "'s commanders SUPERIOR!");
+	}
+	if (message == "makesuperior" && cheatmode)
+	{
+		for (var i in Upgrades[from].Body)
+		{
+			if (Upgrades[from].Body[i].bodyClass === 'Droids' || Upgrades[from].Body[i].bodyClass === 'Cyborgs')
+			{
+				Upgrades[from].Body[i].HitPoints += 500;
+				Upgrades[from].Body[i].HitPointPct += 100;
+				Upgrades[from].Body[i].Armour += 500;
+				Upgrades[from].Body[i].Thermal += 500;
+				Upgrades[from].Body[i].Power += 500;
+			}
+		}
+		console("Made player " + from + "'s units SUPERIOR!");
+	}
+}
+
+function placeOilDrum()
+{
+	var drums = enumFeature(-1, "OilDrum").length;
+	if (drums >= maxOilDrums)
+	{
+		return;
+	}
+
+	var x = syncRandom(mapWidth - 20) + 10;
+	var y = syncRandom(mapHeight - 20) + 10;
+
+	// see if the random position is valid
+	var occupied = (enumRange(x, y, 2, ALL_PLAYERS, false).length > 0);
+	var unreachable = true;
+	for (var i = 0; i < maxPlayers; ++i)
+	{
+		if (propulsionCanReach("wheeled01", x, y, startPositions[i].x, startPositions[i].y))
+		{
+			unreachable = false;
+			break;
+		}
+	}
+	var terrain = terrainType(x, y);
+	if (terrain == TER_WATER || terrain == TER_CLIFFFACE)
+	{
+		unreachable = true;
+	}
+	if (occupied || unreachable)
+	{
+		// try again in a different position after 1 second
+		queue("placeOilDrum", 1000);
+		return;
+	}
+
+	addFeature("OilDrum", x, y);
+}
+
+function eventPickup(feature, droid)
+{
+	if (feature.stattype == OIL_DRUM)
+	{
+		var delay;
+		// generate Geom(1/6) distribution for oil drum respawn delay
+		for (delay = 0; ; ++delay)
+		{
+			if (syncRandom(6) == 0)
+			{
+				break;
+			}
+		}
+		// amounts to 10 minutes average respawn time
+		queue("placeOilDrum", delay * 120000);
+	}
+}
