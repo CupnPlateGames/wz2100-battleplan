798fb5aee8b507769b370e01fc059550cbc9a8d3
Karamel <karamel@cupnplategames.com>
2022-03-30 00:14:56 +0200

    Restore power generator module, add energy cost

--- multiplay/script/rules/setup/structure.js
+++ multiplay/script/rules/setup/structure.js
@@ -6,7 +6,7 @@ function setupStructure(playnum)	// inside hackNetOff()
 	enableStructure("A0VtolPad", playnum);
 	enableStructure("A0ResourceExtractor", playnum);
 	enableStructure("A0PowerGenerator", playnum);
-	//enableStructure("A0PowMod1", playnum);
+	enableStructure("A0PowMod1", playnum);
 	enableStructure("A0ResearchFacility", playnum);
 	enableStructure("A0TankTrap", playnum);
 	enableStructure("LookOutTower", playnum);
